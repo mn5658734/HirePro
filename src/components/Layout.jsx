2550dce79ec1b745febde409196ec74e3d989e3c
milan <milan@bitclass.live>
2026-03-19 01:56:07 +0530

    Initial commit - HirePro blue-collar and pink-collar job hiring platform

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -24,7 +24,6 @@ export default function Layout() {
                 {l.toUpperCase()}
               </button>
             ))}
-            </div>
           </div>
         </div>
         {isStepPage && <ProgressBar />}
